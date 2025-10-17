ed1dc8f05d5d44fbca98552f68f15faf7f4b7814
Ruben Pabon <benru_203@hotmail.com>
2025-10-16 19:44:22 -0500

    chore(DocumentoRepository): se genera el modelo, la migracion para el test,

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+uses(RefreshDatabase::class);
+
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
 }
