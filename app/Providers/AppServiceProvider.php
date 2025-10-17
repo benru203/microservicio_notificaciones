6cd30e35bee27e3efd222c1901bd893742be0336
Ruben Pabon <benru_203@hotmail.com>
2025-10-17 12:05:05 -0500

    chore(id) = se inyectal los repositorio y servicios de documetnoArchivar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Aplicacion\ArchivarDocumento\Interfaces\Servicios\IDocumentoArchivarService;
+use Src\Aplicacion\ArchivarDocumento\Servicios\DocumentoArchivarService;
 use Src\Aplicacion\WebHook\Interfaces\Servicios\IWebHookService;
 use Src\Aplicacion\WebHook\Servicios\WebHookService;
+use Src\Dominio\ArchivarDocumento\Interfaces\Repositorios\IDocumentoArchivarRepository;
 use Src\Dominio\WebHook\Interfaces\Repositorios\IDocumentoRepository;
+use Src\Infraestructura\ArchivarDocumento\Repositorios\DocumentoArchivarRepository;
 use Src\Infraestructura\Repositorios\DocumentoRepository;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(IWebHookService::class, WebHookService::class);
         $this->app->bind(IDocumentoRepository::class, DocumentoRepository::class);
+        $this->app->bind(IDocumentoArchivarService::class, DocumentoArchivarService::class);
+        $this->app->bind(IDocumentoArchivarRepository::class, DocumentoArchivarRepository::class);
     }
 
     /**
